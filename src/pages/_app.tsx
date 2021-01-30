ef07cea9d12385c0fc2efe77104d7df54f797b86
Kousik Manna <kousikmanna9@gmail.com>
2021-01-30 14:57:29 +0530

    add next seo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import { DefaultSeo } from 'next-seo';
+import seo from '../../seo.config';
+import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+    return (
+        <>
+            <DefaultSeo {...seo} />
+            <Component {...pageProps} />
+        </>
+    );
 }
 
-export default MyApp
+export default MyApp;
