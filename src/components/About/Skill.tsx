021b28be45f085aa38c65a5c8323d56c054ccc75
Kousik Manna <kousikmanna9@gmail.com>
2021-06-10 15:29:23 +0530

    add postgresql

--- src/components/About/Skill.tsx
+++ src/components/About/Skill.tsx
@@ -1,35 +1,38 @@
 import style from './Skill.module.css';
 import {
-    SiJavascript,
-    SiTypescript,
-    SiReact,
-    SiGraphql,
-    SiFirebase
+  SiJavascript,
+  SiTypescript,
+  SiReact,
+  SiGraphql,
+  SiFirebase,
+  SiPostgresql
 } from 'react-icons/si';
+
 import { FaNode } from 'react-icons/fa';
 import { ExpressjsIcon, MongodbIcon, NextjsIcon } from '../icons';
 
 function Skill() {
-    return (
-        <div className={style.skill}>
-            <h2>My Skillsets</h2>
-            <div className={style.skill__icons}>
-                <SiJavascript />
-                <SiTypescript />
-                <SiReact />
-                <NextjsIcon />
-            </div>
-            <div className={style.skill__icons}>
-                <FaNode />
-                <ExpressjsIcon />
-                <MongodbIcon />
-                <SiGraphql />
-            </div>
-            <div className={style.skill__icon}>
-                <SiFirebase />
-            </div>
-        </div>
-    );
+  return (
+    <div className={style.skill}>
+      <h2>My Skillsets</h2>
+      <div className={style.skill__icons}>
+        <SiJavascript />
+        <SiTypescript />
+        <SiReact />
+        <NextjsIcon />
+      </div>
+      <div className={style.skill__icons}>
+        <FaNode />
+        <ExpressjsIcon />
+        <MongodbIcon />
+        <SiPostgresql />
+      </div>
+      <div className={style.skill__icon}>
+        <SiGraphql />
+        <SiFirebase />
+      </div>
+    </div>
+  );
 }
 
 export default Skill;
